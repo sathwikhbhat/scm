ad93e8383940f51c1ca7b1e9df464f4aa0c5f85f
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-06-22 16:34:53 +0530

    Initialized Spring Boot application using Spring Initializr

---
+++ src/main/java/com/sathwikhbhat/scm/SCMApplication.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCMApplication.class, args);
+    }
+
+}
