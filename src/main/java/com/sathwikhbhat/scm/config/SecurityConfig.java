3925f45db2c4680a97943ad7005c793997ca8191
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-07-01 10:40:50 +0530

    Added Spring Security dependencies and configuration for user authentication

---
+++ src/main/java/com/sathwikhbhat/scm/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.sathwikhbhat.scm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
