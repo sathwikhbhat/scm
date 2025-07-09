daca41b4abeb3c8508178f38e4bbb1414cfcef8f
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-07-09 11:50:27 +0530

    Added `Add Contacts` page and updated sidebar navigation

---
+++ src/main/java/com/sathwikhbhat/scm/controller/ContactController.java
@@ -0,0 +1,15 @@
+package com.sathwikhbhat.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/contacts")
+public class ContactController {
+
+    @RequestMapping("/add")
+    public String addContactView() {
+        return "user/add-contacts";
+    }
+
+}
--- src/main/java/com/sathwikhbhat/scm/entity/Contacts.java
+++ src/main/java/com/sathwikhbhat/scm/entity/Contacts.java
@@ -1,10 +1,18 @@
 package com.sathwikhbhat.scm.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class Contacts {
 
